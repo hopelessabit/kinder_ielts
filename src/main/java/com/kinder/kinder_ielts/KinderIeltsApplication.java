284d4ec502740a01adc00a0bdb4c40d790096f42
hopelessabit <micalminh1@gmail.com>
2025-02-04 13:31:46 +0700

    Update

--- src/main/java/com/kinder/kinder_ielts/KinderIeltsApplication.java
+++ src/main/java/com/kinder/kinder_ielts/KinderIeltsApplication.java
@@ -1,17 +1,29 @@
 package com.kinder.kinder_ielts;
 
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Map;
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class KinderIeltsApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(KinderIeltsApplication.class);
 	public static void main(String[] args) {
+
+		logger.info("Logging Environment Variables:");
+		Map<String, String> env = System.getenv();
+		for (String envName : env.keySet()) {
+			logger.info("  {}: {}", envName, env.get(envName));
+		}
+		logger.info("End of Environment Variables.");
+
 		SpringApplication.run(KinderIeltsApplication.class, args);
 	}
 
---
+++ src/main/java/com/kinder/kinder_ielts/dto/request/material_link/UpdateMaterialLinkRequest.java
@@ -0,0 +1,4 @@
+package com.kinder.kinder_ielts.dto.request.material_link;
+
+public class UpdateMaterialLinkRequest {
+}
