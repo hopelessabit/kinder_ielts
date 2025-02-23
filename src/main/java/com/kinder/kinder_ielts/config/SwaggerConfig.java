34df272adfbf4280f8df6a4714dbbffe2459eb91
hopelessabit <micalminh1@gmail.com>
2025-02-23 23:05:57 +0700

    Update Swager

--- src/main/java/com/kinder/kinder_ielts/config/SwaggerConfig.java
+++ src/main/java/com/kinder/kinder_ielts/config/SwaggerConfig.java
@@ -3,10 +3,15 @@ package com.kinder.kinder_ielts.config;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * The type Swagger config.
  */
+@Configuration
 @SecurityScheme(name = "Bearer",
         description = "Please enter a valid token",
         scheme = "Bearer",
@@ -14,4 +19,11 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
         bearerFormat = "JWT",
         in = SecuritySchemeIn.HEADER)
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Admin Token")
+                        .description("eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTczOTE4MTI3OSwiZXhwIjoxNzM5Nzg2MDc5fQ.SM7oskkBlobSLD_OFDbZznRHcrKU5K-DRX3WOjyhWPA"));
+    }
 }
