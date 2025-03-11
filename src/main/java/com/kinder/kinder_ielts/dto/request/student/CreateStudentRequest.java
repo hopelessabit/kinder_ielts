5be051c6e999a493891bfe484742a700169e2f34
hopelessabit <micalminh1@gmail.com>
2025-03-11 13:43:53 +0700

    Fix Create Student Request

--- src/main/java/com/kinder/kinder_ielts/dto/request/student/CreateStudentRequest.java
+++ src/main/java/com/kinder/kinder_ielts/dto/request/student/CreateStudentRequest.java
@@ -1,14 +1,20 @@
 package com.kinder.kinder_ielts.dto.request.student;
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class CreateStudentRequest {
+    @NotNull
     private String name;
+    @NotNull
     private String email;
+    @NotNull
     private String phone;
-    private String password;
+    @Nullable
     private String addToCourseId;
+    @Nullable
     private String addToClassroomId;
 }
